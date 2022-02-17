5e40bb985a64a2e79873ce7a11a047d9900c7c59
Shravan <58078248+Sketchy-notabot@users.noreply.github.com>
2022-02-17 11:53:21 +0530

    Time: 66 ms (29.86%), Space: 10.2 MB (71.44%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -5,16 +5,18 @@ public:
         for(int i=0;i<s.length();i++){
             int currRow = startPos[0];
             int currCol = startPos[1];
+            int steps = 0;
             for(int j=i;j<s.length();j++){
                 if(s[j]=='R') currCol++;
                 if(s[j]=='L') currCol--;
                 if(s[j]=='D') currRow++;
                 if(s[j]=='U') currRow--;
                 if(currCol>=0&&currCol<n&&currRow>=0&&currRow<n)
-                    ans[i]++;
+                    steps++;
                 else
                     break;
             }
+            ans[i]=steps;
         }
         return ans;
     }
