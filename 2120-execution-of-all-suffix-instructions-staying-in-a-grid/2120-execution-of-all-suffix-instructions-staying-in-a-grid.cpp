85ca98dbb35b1544975c884d9a9118e29ab70eea
Shravan <58078248+Sketchy-notabot@users.noreply.github.com>
2022-02-17 11:52:23 +0530

    Time: 27 ms (80.40%), Space: 10.1 MB (87.01%) - LeetHub

---
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        vector<int> ans(s.length(), 0);
+        for(int i=0;i<s.length();i++){
+            int currRow = startPos[0];
+            int currCol = startPos[1];
+            int steps = 0;
+            for(int j=i;j<s.length();j++){
+                if(s[j]=='R') currCol++;
+                if(s[j]=='L') currCol--;
+                if(s[j]=='D') currRow++;
+                if(s[j]=='U') currRow--;
+                if(currCol>=0&&currCol<n&&currRow>=0&&currRow<n)
+                    steps++;
+                else
+                    break;
+            }
+            ans[i]=steps;
+        }
+        return ans;
+    }
+};
