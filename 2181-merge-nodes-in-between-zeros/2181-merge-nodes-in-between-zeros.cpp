8c5cf1ca1933191b58b9ca18018160d299e809dd
Shravan <58078248+Sketchy-notabot@users.noreply.github.com>
2022-02-20 11:55:31 +0530

    Time: 1518 ms (5.26%), Space: 290.2 MB (5.26%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -27,7 +27,9 @@ public:
                 } 
                 num=0;
             }
+            ListNode* temp = head;
             head=head->next;
+            delete temp;
         }
         return newHead;
     }
