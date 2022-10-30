4f492116734203f3aebafdac680c4f61e8dae8c7
Shravan <58078248+shravan-d@users.noreply.github.com>
2022-10-31 03:45:01 +0530

    Time: 6 ms (94.90%), Space: 7.4 MB (13.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> hash(26,0);
+        if(s.length()!=t.length())
+            return false;
+        for(int i=0;i<s.length();i++){
+            hash[s[i]-'a']++;
+            hash[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(hash[i])
+                return false;
+        }
+        return true;
+    }
+};
